01d2a8fb5f4620ff326a7a357b93a6324af74406
kristof_crnivec <kristof.crnivec@gmail.com>
2023-11-01 19:55:40 +0100

    Ruby 3.2: Remove deprecated File::exists? method. Use File::exist? instead.

--- lib/tanakai/cli/generator.rb
+++ lib/tanakai/cli/generator.rb
@@ -17,7 +17,7 @@ module Tanakai
 
       def generate_spider(spider_name, in_project:)
         spider_path = in_project ? "spiders/#{spider_name}.rb" : "./#{spider_name}.rb"
-        raise "Spider #{spider_path} already exists" if File.exists? spider_path
+        raise "Spider #{spider_path} already exist?" if File.exist? spider_path
 
         spider_class = to_spider_class(spider_name)
         create_file spider_path do
